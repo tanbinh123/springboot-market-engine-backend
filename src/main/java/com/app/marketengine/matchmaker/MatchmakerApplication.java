a02cda2d3a6ae2dd73561818a0411922b9c8c195
Zaveeth Aslam <zaveethaslam@gmail.com>
2019-03-17 15:14:09 +0530

    added cors support

--- src/main/java/com/app/marketengine/matchmaker/MatchmakerApplication.java
+++ src/main/java/com/app/marketengine/matchmaker/MatchmakerApplication.java
@@ -2,6 +2,10 @@ package com.app.marketengine.matchmaker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class MatchmakerApplication {
@@ -9,5 +13,16 @@ public class MatchmakerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MatchmakerApplication.class, args);
 	}
+	
+	@SuppressWarnings("deprecation")
+	@Bean
+	   public WebMvcConfigurer corsConfigurer() {
+	      return new WebMvcConfigurerAdapter() {
+	         @Override
+	         public void addCorsMappings(CorsRegistry registry) {
+	            registry.addMapping("/api/**").allowedOrigins("http://localhost:4200").allowedOrigins("https://market-engine-ui.herokuapp.com");
+	         }
+	      };
+	   }
 
 }
